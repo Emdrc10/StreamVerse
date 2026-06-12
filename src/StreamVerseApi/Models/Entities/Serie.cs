4ea0c131fa2bc24bb7aed46f1e996707f18e71e0
Emdrc10 <emmanuelduran1004@gmail.com>
2026-06-11 23:04:37 -0400

    adding the serie controller and entity

---
+++ src/StreamVerseApi/Models/Entities/Serie.cs
@@ -0,0 +1,15 @@
+﻿namespace StreamVerseApi.Models.Entities
+{
+    public class Serie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public int Seasons { get; set; }
+        public int Episodes { get; set; }
+        public string Synopsis { get; set; }
+        public string Poster { get; set; }
+        public string Created { get; set; }
+        public string Updated { get; set; }
+    }
+}
